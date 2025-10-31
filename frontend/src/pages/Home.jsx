8e6dddbe197829944ae5203fd9527e510e99bdaf
Fatima <fatima.arab@gmail.com>
2025-10-30 21:00:45 -0400

    Top Doctor section done

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../components/Header'
 import SpecialityMenu from '../components/specialityMenu'
+import TopDoctors from '../components/TopDoctors'
 
 const Home = () => {
   return (
     <div>
       <Header />
       <SpecialityMenu />
+      <TopDoctors />
       
     </div>
   )
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,8 +7,11 @@ export default {
   theme: {
     extend: {
       colors:{
-        'primary':'#5f6FFF',
-      }
+        'primary':'#5f6FFF'
+      },
+      gridTemplateColumns: {
+        'auto': 'repeat(auto-fit, minmax(200px, 1fr))',
+      },
     },
   },
   plugins: [],
