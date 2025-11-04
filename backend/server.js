40e8561b2ef7fdbcad9440f5ce7f81c2626da2b4
Fatima <fatima.arab@gmail.com>
2025-11-03 20:36:03 -0500

    user and doctor model created

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config'
+import connectDB from './config/mongodb.js';
+import { connect } from 'mongoose';
+import connectCloudinary from './config/cloudinary.js';
+
+// app config
+const app = express();
+const port = process.env.PORT || 4000
+connectDB()
+connectCloudinary()
+
+
+// middlewares
+app.use(express.json());
+app.use(cors());
+
+//api endpoints
+app.get('/',(req,res)=>{
+    res.send('API working')
+})
+
+
+
+//To start the server
+app.listen(port, ()=>{
+    console.log(`Server is running on port ${port}`);
+});
